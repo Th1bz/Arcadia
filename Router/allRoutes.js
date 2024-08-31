b4440997ad1f75da60b7521a6af3be17d9a9b32d
Th1bz <thibaultwks@gmail.com>
2024-08-31 09:15:16 +0200

    Création système de routage

---
+++ Router/allRoutes.js
@@ -0,0 +1,8 @@
+import Route from "./Route.js";
+
+//Définir ici vos routes
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),];
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Zoo Arcadia";
