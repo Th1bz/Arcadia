b81535b8fee093964e1d15166e67ac0fbf4411cc
Th1bz <thibaultwks@gmail.com>
2024-09-04 13:07:10 +0200

    add signin and signup pages

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,8 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/services", "Services", "/pages/services.html"),
     new Route("/habitat", "Habitats", "/pages/habitat.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
