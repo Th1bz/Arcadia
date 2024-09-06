b4ecaf73fcf91310543a6c65d98c238d21b26f82
Th1bz <thibaultwks@gmail.com>
2024-09-06 11:18:21 +0200

    add gestion connexion

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/services", "Services", "/pages/services.html"),
     new Route("/habitat", "Habitats", "/pages/habitat.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html", "/js/auth/signin.js"),
     new Route("/signup", "Inscription", "/pages/signup.html", "/js/auth/signup.js"),
 ];
 
---
+++ js/auth/signin.js
@@ -0,0 +1,24 @@
+const passwordInput = document.getElementById("PasswordInput");
+const mailInput = document.getElementById("EmailInput");
+const btnSingin = document.getElementById("btnSignin");
+
+btnSingin.addEventListener("click", checkCredentials);
+
+function checkCredentials(){
+    //Ici, il faudra appeler l'API pour vérifier les credentials en BDD
+    
+    if(mailInput.value == "admin@mail.com" && passwordInput.value == "admin"){
+        alert("Vous êtes connecté")
+        //Il faudra récupérer le vrai token
+        const token = "lkjsdngfljsqdnglkjsdbglkjqskjgkfjgbqslkfdgbskldfgdfgsdgf";
+        setToken(token);
+        //placer ce token en cookie
+
+        // setCookie(RoleCookieName, "admin", 7);
+        window.location.replace("/");
+    }
+    else{
+        mailInput.classList.add("is-invalid");
+        passwordInput.classList.add("is-invalid");
+    }
+}
