811e5a3811b00525dd81879a6b7e9ad12660d26c
Th1bz <thibaultwks@gmail.com>
2024-09-06 11:29:24 +0200

    add gestion déconnexion

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -14,7 +14,7 @@ function checkCredentials(){
         setToken(token);
         //placer ce token en cookie
 
-        // setCookie(RoleCookieName, "admin", 7);
+        setCookie(RoleCookieName, "admin", 7);
         window.location.replace("/");
     }
     else{
--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const tokenCookieName = "accesstoken";
+const signoutBtn = document.getElementById("signout-btn");
 
+signoutBtn.addEventListener("click", signout);
 
+function signout(){
+    eraseCookie(tokenCookieName);
+    window.location.reload();
+}
 
 function setToken(token){
     setCookie(tokenCookieName, token, 7);
